ace9d1563d784398279aac24461cdb34be2cbeaf
kesarwanismriti <smriti.kesarwani@gmail.com>
2024-06-17 20:56:10 +0530

    Updates

--- src/components/Callout.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { css } from "aphrodite";
-import { styles } from "./CalloutStyles";
-
-export default class Callout extends Component {
-  render() {
-    return <div className={css(styles.callout)}>{this.props.children}</div>;
-  }
-}
--- src/components/Rule.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { css } from "aphrodite";
-import { styles } from "./RuleStyles";
-
-export default class Rule extends Component {
-  render() {
-    return <hr className={css(styles.rule)} />;
-  }
-}
